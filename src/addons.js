7fdbfae08e042df4034e41d0a34f844f17f49baf
Tech Capo <techcapo@protonmail.com>
2022-04-21 01:01:49 +0200

    refactor: use modules

---
+++ src/addons.js
@@ -0,0 +1,252 @@
+export default {
+        skellyFace: {
+          offset: [4, 5, 14],
+          dimension: [5, 2, 1],
+          voxels: `
+-B-B- B-B-B`,
+        },
+        camoJacket: {
+          offset: [4, 0, 7],
+          dimension: [5, 2, 5],
+          voxels: `
+ EFG   FGE.
+E   F F   G
+F   E G   F
+G   F F   E
+ FGE   EFG `,
+        },
+        shade: {
+          offset: [1, 8, 7],
+          dimension: [11, 3, 9],
+          voxels: `
+                        H         H
+                        H         H
+                        H         H
+                        H         H
+                        H         H
+                         H       H.
+                         H       H.
+    B B                  H       H.
+              III III    HIIIHIIIH.`,
+        },
+        retro: {
+          offset: [1, 8, 7],
+          dimension: [11, 3, 9],
+          voxels: `
+                        I         I
+                        I         I
+                        I         I
+                        I         I
+                        I         I
+                        I         I
+             I       I   I       I.
+    B B       I     I     I     I..
+               IIIII       IIIII...`,
+        },
+        laser: {
+          offset: [1, 8, 12],
+          dimension: [11, 3, 4],
+          voxels: `
+ H       H   H       H   H       H.
+ H       H   H       H   H       H.
+  H     H     I     I     H     H..
+   HHHHH       IIIII       HHHHH...`,
+        },
+        vape: {
+          offset: [7, 5, 14],
+          dimension: [1, 11, 7],
+          voxels: `
+J . . . . . . . . . .
+J . . . . . . . . . .
+J . . . . . . . . . .
+J . . . . . . . . . .
+J . . . . . . . . . .
+L . . . . . . . . . .
+. . N . N N . N N N N`,
+        },
+        cigarette: {
+          offset: [7, 5, 14],
+          dimension: [1, 11, 7],
+          voxels: `
+J . . . . . . . . . .
+J . . . . . . . . . .
+L . . . . . . . . . .
+L . . . . . . . . . .
+L . . . . . . . . . .
+L . . . . . . . . . .
+. . N . N N . N N N N`,
+        },
+        cigar: {
+          offset: [7, 5, 14],
+          dimension: [1, 11, 7],
+          voxels: `
+J . . . . . . . . . .
+J . . . . . . . . . .
+J . . . . . . . . . .
+J . . . . . . . . . .
+J . . . . . . . . . .
+L . . . . . . . . . .
+. . N . N N . N N N N`,
+        },
+        pipe: {
+          offset: [6, 5, 14],
+          dimension: [3, 11, 9],
+          voxels: `
+ J   .   .   .   .   .   .   .   .   .   ..
+ J   .   .   .   .   .   .   .   .   .   ..
+ J   .   .   .   .   .   .   .   .   .   ..
+ J   .   .   .   .   .   .   .   .   .   ..
+ J   J   .   .   .   .   .   .   .   .   ..
+ J   J   .   .   .   .   .   .   .   .   ..
+ .   .   .   N   .   .   .   .   N   N   ..
+ .   .   .   .   .   N   N   .  NNN NNN  N.
+ .   .   .   .   .   .   .   .   N   N    .`,
+        },
+        fedora: {
+          offset: [0, 13, 1],
+          dimension: [13, 5, 16],
+          voxels: `
+----OOOOO---- ------------- ------------- ------------- -------------
+---OOOOOOO--- ------------- ------------- ------------- -------------
+--OOO   OOO-- -----PPP----- -----OOO----- -----OOO----- ------O------
+-OOO     OOO- ----P   P---- ----O   O---- ----OOOOO---- ----OOOOO----
+OOO       OOO ---P     P--- ---O     O--- ---OOOOOOO--- ---OO---OO---
+OO         OO --P       P-- --O       O-- --OOOOOOOOO-- ---O-OOO-O---
+OO         OO --P       P-- --O       O-- --OOOOOOOOO-- ---O-OOO-O---
+OO         OO --P       P-- --O       O-- --OOOOOOOOO-- ---O-OOO-O---
+OO         OO --P       P-- --O       O-- --OOOOOOOOO-- ---O-OOO-O---
+OO         OO --P       P-- --O       O-- --OOOOOOOOO-- ---OO-O-OO---
+OO         OO --P       P-- --O       O-- ---OOOOOOO--- ----OO-OO----
+OOO       OOO ---P     P--- ---O     O--- ----OOOOO---- -----OOO-----
+-OOO     OOO- ----P   P---- ----O   O---- -----OOO----- ------O------
+--OOO   OOO-- -----PPP----- -----OOO----- ------O------ -------------
+---OOOOOOO--- ------------- ------------- ------------- -------------
+----OOOOO---- ------------- ------------- ------------- -------------`,
+        },
+        boater: {
+          offset: [0, 13, 1],
+          dimension: [13, 4, 16],
+          voxels: `
+----OOOOO---- ------------- ------------- -------------
+---OOOOOOO--- ------------- ------------- -------------
+--OOO   OOO-- -----PPP----- -----OOO----- -----OOO-----
+-OOO     OOO- ----P   P---- ----O   O---- ----OOOOO----
+OOO       OOO ---P     P--- ---O     O--- ---OOOOOOO---
+OO         OO --P       P-- --O       O-- --OOOOOOOOO--
+OO         OO --P       P-- --O       O-- --OOOOOOOOO--
+OO         OO --P       P-- --O       O-- --OOOOOOOOO--
+OO         OO --P       P-- --O       O-- --OOOOOOOOO--
+OO         OO --P       P-- --O       O-- --OOOOOOOOO--
+OO         OO --P       P-- --O       O-- --OOOOOOOOO--
+OOO       OOO ---P     P--- ---O     O--- ---OOOOOOO---
+-OOO     OOO- ----P   P---- ----O   O---- ----OOOOO----
+--OOO   OOO-- -----PPP----- -----OOO----- -----OOO-----
+---OOOOOOO--- ------------- ------------- -------------
+----OOOOO---- ------------- ------------- -------------`,
+        },
+        beanie: {
+          offset: [1, 12, 2],
+          dimension: [11, 5, 14],
+          voxels: `
+---OOOOO--- ---OOOOO--- ----------- ----------- -----------
+--O     O-- --O     O-- ----PPP---- ----------- -----------
+-O       O- -O       O- ---P   P--- ----PPP---- -----------
+O         O O         O --P     P-- ---P   P--- ----PPP----
+O         O O         O -P       P- --P     P-- ---PPPPP---
+O         O O         O -P       P- --P     P-- ---PPPPP---
+O         O O         O -P       P- --P     P-- ---PPPPP---
+O         O O         O -P       P- --P     P-- ---PPPPP---
+O         O O         O -P       P- --P     P-- ---PPPPP---
+O         O O         O -P       P- --P     P-- ---PPPPP---
+-O       O- -O       O- --P     P-- ---P   P--- ----PPP----
+-O       O- -O       O- ---P   P--- ----PPP---- -----------
+--O     O-- --O     O-- ----PPP---- ----------- -----------
+---OOOOO--- ---OOOOO--- ----------- ----------- -----------`,
+        },
+        cap: {
+          offset: [2, 12, 3],
+          dimension: [9, 5, 16],
+          voxels: `
+--OQPOP-- --QOOPO-- ---PQP--- --------- ---------
+-Q     Q- -O     P- --Q   O-- ---OPP--- ---------
+P       O Q       P -P     Q- --PP OP-- ----O----
+Q       P O       O O       P -OO   PO- ---PPO---
+P       O Q       Q P       P -PQ   OP- ---QOP---
+O       Q P       P Q       O -PO   OO- ---OQO---
+Q       P O       O O       P -OP   QP- ---POP---
+O       O P       P O       Q -PO   OQ- ---OPQ---
+P       P Q       Q -P     P- --O   P-- ---QOO---
+Q       O -P     P- -Q     Q- --PP OO-- ----O----
+P       Q -Q     O- --O   O-- ---OPQ--- ---------
+OP     PO --OPPOQ-- ---QOP--- --------- ---------
+PQOQOPOQP --------- --------- --------- ---------
+-OQOPOQP- --------- --------- --------- ---------
+--QPQPO-- --------- --------- --------- ---------
+---OPQ--- --------- --------- --------- ---------`,
+        },
+        capBackwards: {
+          offset: [2, 12, 0],
+          dimension: [9, 5, 16],
+          voxels: `
+   QPO                                          .
+  OPOPQ                                         .
+ PQOPOQO                                        .
+QPOPPOPQP --QOOPO-- ---PQP--- --------- ---------
+OQ     QO -O     P- --O   O-- ---OPP--- ---------
+P       O Q       P -P     Q- --PP OP-- ----O----
+O       P P       O O       P -OO   PO- ---PPO---
+P       O O       P P       P -PQ   OP- ---QOP---
+O       Q P       P Q       O -PO   OO- ---OQO---
+Q       P O       O O       P -OP   QP- ---POP---
+O       O P       P O       Q -PO   OQ- ---OPQ---
+P       P Q       Q -P     P- --O   P-- ---QOO---
+ Q     O  -P     P- -Q     Q- --PP OO-- ----O----
+ P     Q  -O     O- --O   O-- ---OPQ--- ---------
+  POPPO   --OPQOP-- ---QOP--- --------- ---------
+          --------- --------- --------- ---------`,
+        },
+        solanaCap: {
+          offset: [2, 12, 3],
+          dimension: [9, 5, 16],
+          voxels: `
+--QQQQQ-- --QQQQQ-- ---QQQ--- --------- ---------
+-Q     Q- -Q     Q- --Q   Q-- ---QQQ--- ---------
+Q       Q Q       Q -Q     Q- --QQ QQ-- ----Q----
+Q       Q Q       Q Q       Q -QQ   QQ- ---QQQ---
+Q       Q Q       Q Q       Q -QQ   QQ- ---QQQ---
+Q       Q Q       Q P       P -PP   PP- ---PPP---
+Q       Q P       P P       P -PP   PP- ---PPP---
+P       P P       P P       P -PP   PP- ---PPP---
+P       P P       P -P     P- --P   P-- ---PPP---
+O       O -P     P- -P     P- --PP PP-- ----P----
+O       O -P     P- --P   P-- ---PPP--- ---------
+OO     OO --PPPPP-- ---PPP--- --------- ---------
+OOOOOOOOO --------- --------- --------- ---------
+-OOOOOOO- --------- --------- --------- ---------
+--OOOOO-- --------- --------- --------- ---------
+---OOO--- --------- --------- --------- ---------`,
+        },
+        solanaCapBackwards: {
+          offset: [2, 12, 0],
+          dimension: [9, 5, 16],
+          voxels: `
+   OOO                                          .
+  OOOOO                                         .
+ OOOOOOO                                        .
+OOOOOOOOO --QQQQQ-- ---QQQ--- --------- ---------
+OO     OO -Q     Q- --Q   Q-- ---QQQ--- ---------
+O       O Q       Q -Q     Q- --QQ QQ-- ----Q----
+O       O Q       Q Q       Q -QQ   QQ- ---QQQ---
+Q       Q Q       Q Q       Q -QQ   QQ- ---QQQ---
+Q       Q Q       Q P       P -PP   PP- ---PPP---
+Q       Q P       P P       P -PP   PP- ---PPP---
+P       P P       P P       P -PP   PP- ---PPP---
+P       P P       P -P     P- --P   P-- ---PPP---
+ P     P  -P     P- -P     P- --PP PP-- ----P----
+ P     P  -P     P- --P   P-- ---PPP--- ---------
+  PPPPP   --PPPPP-- ---PPP--- --------- ---------
+          --------- --------- --------- ---------`,
+        },
+};
+
+
---
+++ src/components/Viewer.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+  <a-scene id="scene" background="color: #7CE3B6" vr-mode-ui="false" embedded
+           renderer="highRefreshRate:true; logarithmicDepthBuffer:false; colorManagement: false;"
+
+           light="defaultLightsEnabled: true">
+    <a-entity id="camera" camera look-controls="false"></a-entity>
+    <a-entity class="light" visible="true"
+              light="type: hemisphere;  color: #FFF; groundColor: #7BF8C8; intensity: 0.5"></a-entity>
+    <a-entity class="light" visible="true" position="-1 1 1"
+              light="type: directional; color: #FFF; intensity: 0.8;
+                     castShadow:true;  shadowBias:0.001; shadowMapWidth:2048; shadowMapHeight:2048;
+                     shadowCameraVisible:false; shadowCameraNear:0; shadowCameraFar:10;
+                     shadowCameraBottom:-1; shadowCameraLeft:-2; shadowCameraTop:2; shadowCameraRight:2;"></a-entity>
+    <a-entity class="light" visible="true" position="0 1 -1" light="type: directional; color: #FFF; intensity: 1; castShadow:false; shadowBias:0.001;"></a-entity>
+    <a-entity id="container"></a-entity>
+
+
+  </a-scene>
+
+  </main>
+</template>
+
+<style scoped>
+main {
+  margin-left: 30vw;
+  width:  70vw;
+  height: 100vh;
+  padding: 0;
+}
+</style>
