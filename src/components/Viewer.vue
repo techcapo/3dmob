dc16cc9e603968a99acdad143380ccb0f704e958
Tech Capo <techcapo@protonmail.com>
2022-07-05 11:45:34 +0200

    add grid

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,15 +1,21 @@
 <script setup>
+import gridhelper from 'aframe-gridhelper-component'
+
 </script>
 
 <template>
   <main>
-  <a-scene id="scene" background="color: #7CE3B6" vr-mode-ui="false" embedded
+  <a-scene gridhelper="colorGrid: #333; colorCenterLine: #888; size: 100; divisions: 100;" id="scene"
+  background="color: #000000"
+  vr-mode-ui="false" embedded
            renderer="highRefreshRate:true; logarithmicDepthBuffer:false; colorManagement: false;"
 
            light="defaultLightsEnabled: true">
 
 
-    <a-entity id="camera" camera look-controls="false"></a-entity>
+    <a-entity id="camera" camera
+      position="0 1.8 2.5"
+      look-controls="false"></a-entity>
     <a-entity class="light" visible="true"
               light="type: hemisphere;  color: #FFF; groundColor: #7BF8C8; intensity: 0.5"></a-entity>
     <a-entity class="light" visible="true" position="-1 1 1"
@@ -19,7 +25,7 @@
                      shadowCameraBottom:-1; shadowCameraLeft:-2; shadowCameraTop:2; shadowCameraRight:2;"></a-entity>
     <a-entity class="light" visible="true" position="0 1 -1" light="type: directional; color: #FFF; intensity: 1; castShadow:false; shadowBias:0.001;"></a-entity>
     <a-entity id="container"
-    position="0 -0.3 -2"
+    position="0 1.5 0"
     animation="property:rotation; from:0 0 0; to:0 360 0; loop: true; easing:linear; dur:5000"
     >
     </a-entity>
